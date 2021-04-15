4f775c0f94e7ec68c9163b9261daa0e370ea45e2
Kunal Diwan <81952676+kunal1405@users.noreply.github.com>
2021-04-15 12:15:54 +0530

    Update __main__.py

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -17,6 +17,8 @@ from tg_bot.modules import ALL_MODULES
 from tg_bot.modules.helper_funcs.chat_status import is_user_admin
 from tg_bot.modules.helper_funcs.misc import paginate_modules
 
+START_IMG = "https://telegra.ph/file/0C834BA25E8BEC3342144.JPG" 
+
 PM_START_TEXT = """
 hoi {}, my name is {}! if you have any questions about how to use me please give me /help... 
 
